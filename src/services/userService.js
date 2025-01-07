2710d5201484f748f8e02146fecd81d6c805a8c6
luis revilla <lrev4766@gmail.com>
2025-01-07 00:55:01 -0600

    Add user service for authentication tasks

---
+++ src/services/userService.js
@@ -0,0 +1,59 @@
+// src/services/userService.js
+import api from "./api";
+
+/**
+ * Register a new user
+ * POST /users/register
+ */
+export async function registerUser(userData) {
+  // userData might look like: { firstName, lastName, username, email, password, role, gender }
+  const response = await api.post("/users/register", userData);
+  return response.data;
+}
+
+/**
+ * Login user
+ * POST /users/login
+ */
+export async function loginUser(credentials) {
+  // credentials: { email, password }
+  const response = await api.post("/users/login", credentials);
+  return response.data;
+}
+
+/**
+ * Get all users
+ * GET /users
+ */
+export async function getAllUsers() {
+  const response = await api.get("/users");
+  return response.data;
+}
+
+/**
+ * Get user by ID
+ * GET /users/:id
+ */
+export async function getUserById(id) {
+  const response = await api.get(`/users/${id}`);
+  return response.data;
+}
+
+/**
+ * Update user
+ * PUT /users/:id
+ */
+export async function updateUser(id, updatedData) {
+  // updatedData might be { firstName, lastName, username, email, password, etc. }
+  const response = await api.put(`/users/${id}`, updatedData);
+  return response.data;
+}
+
+/**
+ * Delete user
+ * DELETE /users/:id
+ */
+export async function deleteUser(id) {
+  const response = await api.delete(`/users/${id}`);
+  return response.data;
+}
