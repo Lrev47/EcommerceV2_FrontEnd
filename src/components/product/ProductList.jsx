ae9cb0828d7cd5fa6a6cd94dc63ca369290716d6
luis revilla <lrev4766@gmail.com>
2025-01-07 00:44:44 -0600

    Introduce ProductList component for listings

---
+++ src/components/product/ProductList.jsx
@@ -0,0 +1,19 @@
+// src/components/product/ProductList.jsx
+import React from "react";
+import ProductCard from "./ProductCard";
+
+const ProductList = ({ products }) => {
+  if (!products || products.length === 0) {
+    return <div>No products found.</div>;
+  }
+
+  return (
+    <div className="product-list">
+      {products.map((p) => (
+        <ProductCard key={p.id} product={p} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
