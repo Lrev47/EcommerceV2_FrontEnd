cbd71381903a20e942b3323938e0c82fc974c07d
luis revilla <lrev4766@gmail.com>
2025-01-08 00:09:36 -0600

    adding reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,20 +4,20 @@ import userReducer from "./slices/userSlice";
 import productReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartSlice";
 // Import other slices as you create them:
-// import orderReducer from "./slices/orderSlice";
-// import reviewReducer from "./slices/reviewSlice";
-// import paymentReducer from "./slices/paymentSlice";
-// import addressReducer from "./slices/addressSlice";
+import orderReducer from "./slices/orderSlice";
+import reviewReducer from "./slices/reviewSlice";
+import paymentReducer from "./slices/paymentSlice";
+import addressReducer from "./slices/addressSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     products: productReducer,
     cart: cartReducer,
-    // order: orderReducer,
-    // reviews: reviewReducer,
-    // payments: paymentReducer,
-    // address: addressReducer,
+    orders: orderReducer,
+    reviews: reviewReducer,
+    payment: paymentReducer,
+    address: addressReducer,
   },
 });
 
