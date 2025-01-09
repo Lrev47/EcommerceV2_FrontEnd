4eb7222b699082434daf7e4a52fe43f7a9c78f86
luis revilla <lrev4766@gmail.com>
2025-01-08 21:19:27 -0600

    added logout and route to account

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,13 +2,15 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchAllProducts } from "../../redux/slices/productSlice"; // import your thunk
+import { fetchAllProducts } from "../../redux/slices/productSlice";
+
+// 1) Import the logout action
+import { logout } from "../../redux/slices/userSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
 
-  // We fetch all products to get categories (assuming it's cheap enough).
-  // If you already fetch them on some parent, you can skip or conditionally fetch.
+  // We fetch all products to get categories (if your code does that).
   useEffect(() => {
     dispatch(fetchAllProducts());
   }, [dispatch]);
@@ -16,11 +18,15 @@ const Header = () => {
   const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   const { userInfo } = useSelector((state) => state.user);
 
-  // Get product items from store
+  // Get product items from store for categories
   const products = useSelector((state) => state.products.items);
-
-  // Derive unique categories
   const categories = [...new Set(products.map((p) => p.category))];
+
+  // 2) Handle logout
+  const handleLogout = () => {
+    dispatch(logout());
+    // Optionally redirect or do anything else you want on logout
+  };
 
   return (
     <header>
@@ -30,12 +36,10 @@ const Header = () => {
           <Link to="/">My Brand</Link>
         </div>
 
-        {/* Navigation Links */}
         <ul className="nav-links">
-          {/* PRODUCTS with dropdown */}
+          {/* PRODUCTS with dropdown of categories */}
           <li className="dropdown">
             <Link to="/products">Products</Link>
-            {/* Hover dropdown showing categories */}
             <div className="dropdown-content">
               {categories.length > 0 ? (
                 categories.map((cat) => (
@@ -51,27 +55,38 @@ const Header = () => {
             </div>
           </li>
 
-          {/* Show these links ONLY if NOT logged in */}
+          {/* If user is NOT logged in => show Register/Login */}
           {!userInfo && (
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
+            <>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
           )}
 
-          {/* Show these links ONLY if logged in */}
-          {userInfo && <li>{/* <Link to="/admin">Admin</Link> */}</li>}
+          {/* If user is logged in => (Optional) show Admin link, etc. */}
+          {userInfo && (
+            <>
+              {/* Example: If user is admin, show admin link:
+                <li><Link to="/admin">Admin</Link></li>
+              */}
+            </>
+          )}
 
-          {/* Cart link with quantity */}
+          {/* CART link */}
           <li>
             <Link to="/cart" className="cart-link">
               Cart ({cartQuantity})
             </Link>
           </li>
 
-          {/* Profile image or login button on the far right */}
-          <li>
-            {userInfo ? (
-              <Link to="/account" className="profile-link">
+          {/* Profile Dropdown for logged in user */}
+          {userInfo && (
+            <li className="profile-dropdown">
+              <div className="profile-link">
                 <img
                   src={
                     userInfo.userImageUrl
@@ -81,11 +96,15 @@ const Header = () => {
                   alt="Profile"
                   className="profile-image"
                 />
-              </Link>
-            ) : (
-              <Link to="/login">Login</Link>
-            )}
-          </li>
+              </div>
+
+              {/* The dropdown that appears on hover */}
+              <div className="profile-dropdown-content">
+                <Link to="/account">My Account</Link>
+                <button onClick={handleLogout}>Logout</button>
+              </div>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
